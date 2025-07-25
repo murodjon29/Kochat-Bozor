77602592201143e26f447136dc2bea498583284f
murodjon29 <smurodjon983@gmail.com>
2025-07-25 22:15:44 +0500

    updated

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,9 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Product } from 'src/saller/entities/product.entiti';
 import { User } from 'src/user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Product, User])],
+  imports: [TypeOrmModule.forFeature([Order, Product, User]),
+
+  JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '1h' },
+        }),
+      }),
+],
   controllers: [OrderController],
   providers: [OrderService],
 })
