604ab5fb5e8f2bf14bf90e72e45a312cd1a1fe0c
murodjon29 <smurodjon983@gmail.com>
2025-08-04 12:27:10 +0500

    updated

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,9 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Product } from 'src/saller/entities/product.entiti';
 import { User } from 'src/user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Product, User])],
+  imports: [TypeOrmModule.forFeature([Order, Product, User]), 
+  JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '1d' },
+  })
+],
   controllers: [OrderController],
   providers: [OrderService],
 })
