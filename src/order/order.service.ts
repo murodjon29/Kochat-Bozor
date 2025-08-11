0548da41e753d71cc216924175a067977bdc95d6
murodjon29 <smurodjon983@gmail.com>
2025-08-11 12:46:16 +0500

    updated

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -8,7 +8,7 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
-import { In, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Product } from 'src/saller/entities/product.entiti';
 import { User } from 'src/user/entities/user.entity';
 
@@ -23,26 +23,24 @@ export class OrderService {
   async create(createOrderDto: CreateOrderDto) {
     try {
       const { productId, quantity, userId } = createOrderDto;
+
       const product = await this.productRepository.findOne({
         where: { id: productId },
       });
+      if (!product) throw new NotFoundException('Product not found');
+
       const user = await this.userRepository.findOne({ where: { id: userId } });
       if (!user) throw new NotFoundException('User not found');
-      if (!product) throw new NotFoundException('Product not found');
-      if (product.stock < quantity)
-        throw new BadRequestException(
-          `Zaxirada yetarli mahsulot yo'q: ${product.name}. Mavjud: ${product.stock}, So'ralgan: ${quantity}`,
-        );
-      product.stock -= quantity;
-      await this.productRepository.update(product.id, product);
+
       const totalPrice = product.price * quantity;
+
       const order = this.orderRepository.create({
         product,
         quantity,
         totalPrice,
         user: { id: userId },
       });
-      await this.productRepository.save(product);
+
       return await this.orderRepository.save(order);
     } catch (error) {
       throw new InternalServerErrorException(
@@ -53,10 +51,9 @@ export class OrderService {
 
   async findAll() {
     try {
-      const orders = await this.orderRepository.find({
+      return await this.orderRepository.find({
         relations: ['product', 'user'],
       });
-      return orders;
     } catch (error) {
       throw new InternalServerErrorException(
         `Error finding orders: ${error.message}`,
@@ -66,26 +63,40 @@ export class OrderService {
 
   async findOne(id: number) {
     try {
-      const order = await this.orderRepository.findOne({ where: { id } });
+      const order = await this.orderRepository.findOne({
+        where: { id },
+        relations: ['product', 'user'],
+      });
       if (!order) throw new NotFoundException(`Order not found id: ${id}`);
       return order;
     } catch (error) {
       throw new InternalServerErrorException(
-        `Orderni topishda error: ${error.message}`,
+        `Orderni topishda xato: ${error.message}`,
       );
     }
   }
 
   async update(id: number, updateOrderDto: UpdateOrderDto) {
     try {
-      const order = await this.orderRepository.findOne({ where: { id } });
-      if (!order) throw new NotFoundException(`Order not found id: ${id}`);
-      await this.orderRepository.update(id, updateOrderDto);
-      const updatedOrder = await this.orderRepository.findOne({
+      const order = await this.orderRepository.findOne({
         where: { id },
         relations: ['product', 'user'],
       });
-      return updatedOrder;
+      if (!order) throw new NotFoundException(`Order not found id: ${id}`);
+      if (updateOrderDto.status) {
+        if (updateOrderDto.status === 'completed') {
+          if (order.product.stock < order.quantity) {
+            throw new BadRequestException(
+              `Zaxirada yetarli mahsulot yo'q: ${order.product.name}. Mavjud: ${order.product.stock}, So'ralgan: ${order.quantity}`,
+            );
+          }
+          order.product.stock -= order.quantity;
+          await this.productRepository.save(order.product);
+        }
+        order.status = updateOrderDto.status;
+      }
+
+      return await this.orderRepository.save(order);
     } catch (error) {
       throw new InternalServerErrorException(
         `Orderni yangilashda xato: ${error.message}`,
